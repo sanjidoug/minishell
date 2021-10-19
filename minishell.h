87c1966eae7af026bd31bbc48b086d6348871e9d
Dougan Lescart <dlescart@shi-r10-p5.s19.be>
2021-10-19 19:28:17 +0200

    few builtins implemented and function cat and function exit

--- minishell.h
+++ minishell.h
@@ -6,19 +6,17 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-typedef struct s_utils {
-    char *str;
-    int len_bequal;
-}               t_utils;
-
-/*typedef struct s_parse {
-
-}*/
+typedef struct s_parse {
+    char **tab_arg;
+    char **tab_cmd;
+    char *cont_env;
+    char **tab_path;
+    char *path;
+}               t_parse;
 
 char			**ft_split(const char *str, char c);
 void ft_putstr(char *str);
 int	ft_strcmp(char *s1, char *s2);
-char	*ft_strdup_env(char *src, t_utils utils);
 char *ft_strcat(char *dest, char *src);
 char *ft_strcpy(char *dest, char *src);
 char	*ft_strjoin_path(char const *s1, char const *s2);
--- parsing.c
+++ parsing.c
@@ -1,41 +1,6 @@
 #include "minishell.h"
 #include <stdio.h>
 
-char *my_getenv(char **env, char *elem)
-{
-    t_utils utils;
-    int i;
-    int j;
-    char *cont_env;
-    
-    cont_env = 0;
-    i = 0;
-    while(env[i] != NULL)
-    {  
-        if (env[i][0] == elem[0])
-        {
-            utils.len_bequal = 0;
-            j = 0;
-            while(env[i][j++] != '=')
-                utils.len_bequal++;
-            utils.str = malloc(sizeof(char) * utils.len_bequal);
-            j = 0;
-            while(j < utils.len_bequal)
-            {
-                utils.str[j] = env[i][j];
-                j++;
-            }
-            utils.str[j] = '\0';
-            if (!ft_strcmp(utils.str, elem))
-                break; 
-        }
-        i++;
-    }
-    if(env[i] != NULL)
-        cont_env = ft_strdup_env(env[i], utils);
-    return(cont_env);
-}
-
 char *add_cmd_to_path(char *cmd, char **tab_path)
 {
     int i;
@@ -43,10 +8,17 @@ char *add_cmd_to_path(char *cmd, char **tab_path)
     char *path;
 
     i = 0;
+    path = "";
     while (tab_path[i] != NULL)
     {
+        path = ft_strjoin_path(path, cmd);
+        fd = open(path, O_RDONLY);
+        if (fd >= 0)
+        {
+            close(fd);
+            return(path);
+        }
         path = ft_strjoin_path(tab_path[i], cmd);
-        path[ft_strlen(path) - 1] = 0;
         fd = open(path, O_RDONLY);
         if (fd >= 0)
         {
@@ -59,43 +31,67 @@ char *add_cmd_to_path(char *cmd, char **tab_path)
     return (NULL);
 }
 
+void ft_free(t_parse *parse)
+{
+    int i;
+
+    i = 0;
+    while (parse->tab_path[i] != NULL)
+        free(parse->tab_path[i++]);
+    free(parse->tab_path);
+    i = 0;
+    while(parse->tab_cmd[i] != NULL)
+        free(parse->tab_cmd[i++]);
+    free(parse->tab_cmd);
+}
+
+void ft_cat(t_parse *parse)
+{
+    int i;
+
+    i = 1;
+    while(parse->tab_arg[i] != NULL)
+    {
+        if(parse->tab_arg[i][0] != '-')
+            parse->tab_arg[i] = ft_strjoin_path(getenv("PWD"), parse->tab_arg[i]);
+        i++;
+    }
+}
+
 int main(int ac, char **ar, char **env)
 {
-    char **tab_arg;
-    char **tab_cmd;
+    t_parse parse;
     int i;
-    char *cont_env;
-    char **tab_path;
-    char *path;
-
-    /*tab_arg = ft_split(get_next_line(0), ' ');
-    cont_env = my_getenv(env, "PATH");
-    path = ft_split(cont_env, ':');
-    i = 0;
-    while(path[i] != NULL)
-    {
-        printf("%s\n", path[i]);
-        i++;
-    }*/
-    //printf("%d\n", execve("/Users/dlescart/.brew/bin", tab_arg, env));
+    int status;
+    
     while(1)
     {   
         i = 0;
         ft_putstr("my_prompt>");
-        cont_env = getenv("PATH");
-        tab_path = ft_split(cont_env, ':');
-        tab_cmd = ft_split(get_next_line(0), '|');
-        while (tab_cmd[i] != NULL)
+        parse.cont_env = getenv("PATH");
+        parse.tab_path = ft_split(parse.cont_env, ':');
+        parse.tab_cmd = ft_split(get_next_line(0), '|');
+        if (!ft_strcmp(parse.tab_cmd[0], "exit\n"))
+        {
+            ft_free(&parse);
+            exit(0);
+        }
+        while (parse.tab_cmd[i] != NULL)
         {
             if(fork() == 0)
-            {
-                tab_arg = ft_split(tab_cmd[i], ' ');
-                tab_arg[0] = add_cmd_to_path(tab_arg[0], tab_path);
-                execve(tab_arg[0], tab_arg, env);
+            { 
+                if (parse.tab_cmd[i][ft_strlen(parse.tab_cmd[i]) - 1] == '\n')
+                   parse.tab_cmd[i][ft_strlen(parse.tab_cmd[i]) - 1] = 0;
+                parse.tab_arg = ft_split(parse.tab_cmd[i], ' ');
+                if (!ft_strcmp(parse.tab_arg[0],"cat"))
+                    ft_cat(&parse);
+                parse.tab_arg[0] = add_cmd_to_path(parse.tab_arg[0], parse.tab_path);
+                execve(parse.tab_arg[0], parse.tab_arg, env);
             }
             else
                 i++;
+            wait(&status);
         }
-        //printf("%s\n", path);
+        ft_free(&parse);
     }
 }
